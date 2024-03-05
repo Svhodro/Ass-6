7ec4c5bf0ae92d6179f678f2efd79b94fb4c4c29
Svhodro <rudrolipi@gmail.com>
2024-03-05 10:13:31 +0600

    commit

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ let cont = document.querySelector(".conent")
 let fatchData;
 
 //
-let count=1;
+let count = 0;
+
 
 
 
@@ -16,16 +17,16 @@ let count=1;
 async function datafatch(all, valu) {
   const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?${all + valu}`);
   let data = await res.json();
-  
+
 
 
   allSearch.textContent = "";
   data.posts.forEach(item => {
     const div = document.createElement('div');
     div.innerHTML = `
-<div class="cerd flex gap-8 bg-slate-200 h-72 rounded-lg p-4">
+<div class="cerd flex lg:flex-row flex-col gap-8 bg-slate-200 lg:h-72 rounded-lg p-4">
 <div class="avtar ">
-  <div class="avatar w-20">
+  <div class="avatar lg:w-20">
 
     <div class="w-24 rounded-xl">
       <img src="${item.image}" />
@@ -72,6 +73,7 @@ async function datafatch(all, valu) {
 `
     allSearch.appendChild(div)
     count++
+
   });
   // console.log(data)
   // element creation
@@ -90,8 +92,8 @@ const latestPost = async () => {
 
   data.forEach(item => {
 
-    const div = document.createElement('div');
-    div.innerHTML = `<div class="card w-96 bg-base-300 shadow-xl">
+    const div = document.createElement('div');    
+    div.innerHTML = `<div class="card  bg-base-300 shadow-xl">
           <figure class="p-4"><img src="${item.cover_image}" alt="Shoes" /></figure>
           <div class="card-body">
             <h2 class="text-lg"><i class="fa-regular fa-calendar-check"></i> <span>${item.author.posted_date ? item.author.posted_date : 'No Publish date'}</span></h2>
@@ -131,11 +133,15 @@ console.log(fatchData);
 searchBtn.addEventListener('click', async (all, valu) => {
   const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?${all + valu}`);
   let fatch = await res.json();
+
   allSearch.textContent = "";
+
+
   fatch.posts.forEach(item => {
-    
     if (item.category === searchValue.value) {
-      
+
+
+
       const div = document.createElement('div');
       div.innerHTML = `
 <div class="cerd flex gap-8 bg-slate-200 h-72 rounded-lg p-4">
@@ -185,43 +191,47 @@ searchBtn.addEventListener('click', async (all, valu) => {
 </div>
 </div>
 `
-allSearch.appendChild(div)
-    }else{
-      
+      allSearch.appendChild(div)
+
     }
   })
 
-  });
-setTimeout(()=>{
-  let btn=document.querySelectorAll(".tab")
+
+});
+let a=0;
+setTimeout(() => {
+  let btn = document.querySelectorAll(".tab")
   // console.log(btn);
-  btn.forEach(item=>{
-    item.addEventListener('click',()=>{
-      let id= item.classList[1]    
-      let dataArray=document.getElementsByClassName(`${id}`) 
-      let title=dataArray[0].innerHTML;
-      let see =dataArray[1].innerHTML;
-      let div=document.createElement('div');
-      div.innerHTML=`
-      <div class="flex gap-3 bg-slate-50 rounded-lg p-3">
-            <div class="font-bold">
-              <p>${title}</p>
-            </div>
-            <div>
-              <p class="flex text-center gap-2"><i class="fa-regular fa-eye m-1"></i> <span>${see}</span></p>
-
-            </div>
-
-          </div>
-      `
-      cont.appendChild(div);
-   
-
-
-
-    })
-  }
-
-  )
+ 
+  btn.forEach(item => {
+    item.addEventListener('click', () => {
+      let id = item.classList[1]
+      let dataArray = document.getElementsByClassName(`${id}`)
+      let title = dataArray[0].innerHTML;
+      let see = dataArray[1].innerHTML;
+      let div = document.createElement('div');
+      div.innerHTML = `
+        <div class="flex gap-3 bg-slate-50 rounded-lg p-3">
+              <div class="font-bold">
+                <p>${title}</p>
+              </div>
+              <div>
+                <p class="flex text-center gap-2"><i class="fa-regular fa-eye m-1"></i> <span>${see}</span></p>
   
-},2000)
+              </div>
+  
+            </div>
+        `
+      cont.appendChild(div);
+    a++;
+    let change=document.querySelector(".row");
+    change.innerHTML=a;
+    })
+  })
+
+}, 2000)
+
+
+
+
+
